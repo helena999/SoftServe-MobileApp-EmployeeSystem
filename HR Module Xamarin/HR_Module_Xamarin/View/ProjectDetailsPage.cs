927b40d6a1677af4012be55332989f6cf688d757
SVladkov <s.v.vladkov@gmail.com>
2016-03-21 00:17:36 +0200

    Added project details page

---
+++ HR Module Xamarin/HR_Module_Xamarin/View/ProjectDetailsPage.cs
@@ -0,0 +1,58 @@
+﻿using HR_Module_Xamarin.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using HR_Module_Xamarin.View;
+
+namespace HR_Module_Xamarin
+{
+    class ProjectDetailsPage : ContentPage
+    {
+        private Project item;
+
+        public ProjectDetailsPage(Project initialItem)
+        {
+            Title = "Project Details Page";
+
+            this.item = initialItem;
+
+            Label nameLabel = new Label
+            {
+                Text = "Name",
+                TextColor = Color.Green,
+                FontSize = 15
+            };
+            
+            Label name = new Label
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Text = item.Name,
+                FontSize = 30
+            };
+
+            Label projectManagerLabel = new Label
+            {
+                Text = "Project manager",
+                TextColor = Color.Green,
+                FontSize = 15
+            };
+
+            Label projectManager = new Label
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Text = item.ProjectManager.Name,
+                FontSize = 30
+            };
+
+            var stackLayout = new StackLayout
+            {
+                Children = { nameLabel, name, projectManagerLabel, projectManager }
+            };
+
+            this.Content = stackLayout;
+        }
+    }
+}
