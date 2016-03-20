051921b66306aab79c8a77d709b6e089f43dd7b8
dido <didobatev@abv.bg>
2016-03-20 11:32:54 +0200

    projects

---
+++ HR Module Xamarin/HR_Module_Xamarin/View/ProjectDetailsPage.cs
@@ -0,0 +1,115 @@
+﻿using HR_Module_Xamarin.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using HR_Module_Xamarin.View;
+
+namespace HR_Module_Xamarin
+{
+    class ProjectDetailsPage : ContentPage
+    {
+        private Project item;
+
+
+        public ProjectDetailsPage(Project item)
+        {
+            Title = "Project Details Page";
+
+
+
+            this.item = item;
+            Label name = new Label
+            {
+                Text = "Name",
+                TextColor = Color.Green,
+                FontSize = 15
+            };
+            Label nameLabel = new Label
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Text = item.Name,
+                FontSize = 30
+            };
+
+            Label projectManager = new Label
+            {
+                Text = "Project Manager",
+                TextColor = Color.Green,
+                FontSize = 15
+            };
+            Label projectManagerLabel = new Label
+            {
+                Text = item.ProjectManager.ToString(),
+                FontSize = 30
+            };
+            Label teamLead = new Label
+            {
+                Text = "Team Leader",
+                TextColor = Color.Green,
+                FontSize = 15
+
+            };
+            Label temLeadLabel = new Label
+            {
+                Text = item.TeamLeader.ToString(),
+                FontSize = 30
+            };
+            //Label projectLabel = new Label
+            //{
+            //    Text = item.ProjectName,
+            //    FontSize = 30
+            //};
+
+            //Label managerLabel = new Label
+            //{
+            // Text = item.Manager.ToString(),
+            //    FontSize = 30
+            //};
+            //Label position = new Label
+            //{
+            //    Text = "Position",
+            //    TextColor = Color.Green,
+            //    FontSize = 15
+
+            //};
+            //Label positionLabel = new Label
+            //{
+            //    Text = item.Position.Name,
+            //    FontSize = 30
+            //};
+            //Label salary = new Label
+            //{
+            //    Text = "Salary",
+            //    TextColor = Color.Green,
+            //    FontSize = 15
+
+            //};
+            //Label salaryLabel = new Label
+            //{
+            //    Text = item.Salary,
+            //    FontSize = 30
+            //};
+            //Label phone = new Label
+            //{
+            //    Text = "Phone",
+            //    TextColor = Color.Green,
+            //    FontSize = 15
+
+            //};
+            //Label phoneLabel = new Label
+            //{
+            //    Text = item.Phone.ToString(),
+            //    FontSize = 30
+            //};
+            var stackLayout = new StackLayout
+            {
+                Children = { name, nameLabel, projectManager, projectManagerLabel, teamLead, temLeadLabel, /*projectLabel*//*,managerLabel, *//*position, positionLabel, phone, phoneLabel, salary, salaryLabel*/ }
+            };
+
+            this.Content = stackLayout;
+        }
+    }
+}
---
+++ HR Module Xamarin/HR_Module_Xamarin/View/ProjectsPage.cs
@@ -0,0 +1,82 @@
+﻿using HR_Module_Xamarin.Model;
+using HR_Module_Xamarin.View;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HR_Module_Xamarin
+{
+    public class ProjectsPage : ContentPage
+    {
+        ListView listView;
+        public ProjectsPage()
+        {
+            Title = "Projects";
+
+            listView = new ListView();
+            listView.ItemTemplate = new DataTemplate
+                    (typeof(ProjectCell));
+            listView.ItemTapped += async (sender, e) =>
+            {
+                Project item = (Project)e.Item;
+                await Navigation.PushAsync(new ProjectDetailsPage(item));
+
+
+            };
+            Button createProject = new Button
+            {
+                Text = "Create project"
+            };
+            createProject.Clicked += async (sender, args) =>
+            await Navigation.PushAsync(new ProjectCreatePage());
+
+            var layout = new StackLayout();
+            layout.Children.Add(listView);
+            layout.Children.Add(createProject);
+            layout.VerticalOptions = LayoutOptions.FillAndExpand;
+            Content = layout;
+
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            ((App)App.Current).ResumeAtTodoId = -1;
+            listView.ItemsSource = App.Database.GetProject();
+        }
+    }
+    public class ProjectCell : ViewCell
+    {
+        public ProjectCell()
+        {
+            var name = new Label
+            {
+
+                HorizontalOptions = LayoutOptions.Start,
+                FontSize = 20
+            };
+            name.SetBinding(Label.TextProperty, "Name");
+
+            var position = new Label
+            {
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                FontSize = 20,
+                TextColor = Color.Green
+            };
+            position.SetBinding(Label.TextProperty, "Project.Name");
+
+            var layout = new StackLayout
+            {
+                Spacing = 0,
+                Orientation = StackOrientation.Horizontal,
+                Children = { name, position }
+            };
+            View = layout;
+        }
+    }
+}
