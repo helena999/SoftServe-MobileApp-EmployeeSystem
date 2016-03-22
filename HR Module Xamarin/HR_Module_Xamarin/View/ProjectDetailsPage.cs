b7c564047f3f0af2cbf34c997b3427180ccaf04d
SVladkov <s.v.vladkov@gmail.com>
2016-03-22 23:30:33 +0200

    Minor merge in project details page

--- HR Module Xamarin/HR_Module_Xamarin/View/ProjectDetailsPage.cs
+++ HR Module Xamarin/HR_Module_Xamarin/View/ProjectDetailsPage.cs
@@ -12,8 +12,7 @@ namespace HR_Module_Xamarin
     class ProjectDetailsPage : ContentPage
     {
         private Project item;
-
-<<<<<<< HEAD
+        
         public ProjectDetailsPage(Project initialItem)
         {
             Title = "Project Details Page";
@@ -21,35 +20,20 @@ namespace HR_Module_Xamarin
             this.item = initialItem;
 
             Label nameLabel = new Label
-=======
 
-        public ProjectDetailsPage(Project item)
-        {
-            Title = "Project Details Page";
-
-
-
-            this.item = item;
-            Label name = new Label
->>>>>>> origin/problem_with_project_details
             {
                 Text = "Name",
                 TextColor = Color.Green,
                 FontSize = 15
             };
-<<<<<<< HEAD
-            
+
             Label name = new Label
-=======
-            Label nameLabel = new Label
->>>>>>> origin/problem_with_project_details
             {
                 VerticalOptions = LayoutOptions.Center,
                 Text = item.Name,
                 FontSize = 30
             };
-
-<<<<<<< HEAD
+            
             Label projectManagerLabel = new Label
             {
                 Text = "Project manager",
@@ -67,23 +51,6 @@ namespace HR_Module_Xamarin
             var stackLayout = new StackLayout
             {
                 Children = { nameLabel, name, projectManagerLabel, projectManager }
-=======
-            Label projectManager = new Label
-            {
-                Text = "Project Manager",
-                TextColor = Color.Green,
-                FontSize = 15
-            };
-            Label projectManagerLabel = new Label
-            {
-                Text = item.ProjectManager.Name,
-                FontSize = 30
-            };
-            
-            var stackLayout = new StackLayout
-            {
-                Children = { name, nameLabel, projectManager, projectManagerLabel }
->>>>>>> origin/problem_with_project_details
             };
 
             this.Content = stackLayout;
